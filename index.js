af23419fcf7c5f3e8ec1c50cdb3edc2d3530ec52
iSamBa <bahou.aissam@gmail.com>
2021-01-09 22:11:51 +0100

    add cors

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import express from "express";
+import cors from "cors";
 import postsRouter from "./src/routes/posts-routes.js"
 import usersRouter from "./src/routes/users-routes.js"
-
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors());
 
 app.use('/users', usersRouter)
 app.use('/posts', postsRouter)
-
 
 app.listen(PORT, () => {
     console.log(`Server started listening on port ${PORT}`);
